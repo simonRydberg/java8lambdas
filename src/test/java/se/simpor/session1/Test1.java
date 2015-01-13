81647c76adbb1817133caffcaf0940cd1ca79090
Simon Rydberg <simon.rydberg@transrail.se>
2015-01-13 14:41:03 +0100

    Initial commit

---
+++ src/test/java/se/simpor/session1/Test1.java
@@ -0,0 +1,16 @@
+package se.simpor.session1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by simonrydberg on 13/01/15.
+ */
+public class Test1 {
+
+    @Test
+    public void sucess(){
+        Assert.assertEquals(2, 2);
+    }
+
+}
