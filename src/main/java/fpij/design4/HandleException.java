d53599374907a7f8946d35656eb9855b2c1a4f28
Simon Rydberg <simon.rydberg@transrail.se>
2015-01-27 15:49:52 +0100

    Session 2 - kap4

---
+++ src/main/java/fpij/design4/HandleException.java
@@ -0,0 +1,27 @@
+/***
+ * Excerpted from "Functional Programming in Java",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/vsjava8 for more book information.
+ ***/
+package fpij.design4;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.stream.Stream;
+
+public class HandleException {
+    public static void main(String[] args) throws IOException {
+        Stream.of("/usr", "/tmp")
+                .map(path -> {
+                    try {
+                        return new File(path).getCanonicalPath();
+                    } catch (IOException ex) {
+                        return ex.getMessage();
+                    }
+                })
+                .forEach(System.out::println);
+    }
+}
